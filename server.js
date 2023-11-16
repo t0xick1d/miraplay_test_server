7f9f4385286d40f85275b5e8df6b923cc691ea8d
zheniaStukalo <82517499+t0xick1d@users.noreply.github.com>
2023-11-16 13:05:36 +0200

    created repositories

---
+++ server.js
@@ -0,0 +1,19 @@
+const app = require('./app');
+const mongoose = require('mongoose');
+
+const { DB_HOST, PORT = 3000 } = process.env;
+
+// password: RtNHTvB1d1xyHjKV
+
+mongoose.set('strictQuery', true);
+
+mongoose
+   .connect(DB_HOST)
+   .then(() => {
+      app.listen(PORT);
+      console.log('Database connection successful');
+   })
+   .catch((error) => {
+      console.log(error.message);
+      process.exit(1);
+   });
